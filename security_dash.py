0bac6859e8fe305b5e695eee392d703aafc5892e
pothospeithos <calvinkvkpadayao@gmail.com>
2022-08-07 13:13:55 -0600

    updated comments with sphinx markup

--- security_dash.py
+++ security_dash.py
@@ -3,10 +3,13 @@ import webbrowser
 import requests
 
 
-# Gather news from cybersecurity news websites.
 def news():
-    # Dict containing predetermined websites to pull news from.
-    # Can be modded to add more websites.
+    """
+    Gather user input to determine what website they would like to gather
+    information from. User can select number of articles to view in terminal
+    and open in their browser.
+    """
+
     websites = {
         "TheHackerNews": "https://feeds.feedburner.com/TheHackersNews",
         "ThreatPost": "https://threatpost.com/feed",
@@ -14,21 +17,16 @@ def news():
         "PortSwigger": "https://portswigger.net/daily-swig/us",
     }
 
-    # Print website urls to visit.
     print('-----Security News Websites-----')
     for k in websites.keys():
         print(f'- {k}')
 
-    # Try user's input, and return exception if it does not match website  in dict websites.
-    # Temporarily stores article names and titles from website into respective arrays article_list and article_link.
     try:
-        # Get user input for which site to view
         web_input = str(input('Enter website name to view articles: '))
         news_feed = feedparser.parse(websites[web_input])
         article_list = []
         article_link = []
         num = int(input('Please enter amount of articles to view from the selected website: '))
-        # Append article links and titles to respective lists
         for i in range(num):
             article = news_feed.entries[i]
             titles = article.title
@@ -36,67 +34,59 @@ def news():
             article_link.append(link)
             article_list.append(titles)
 
-        # Print article's number, and the article title
         article_num = 1
         for article in article_list:
             print('[{}] {}'.format(str(article_num), article))
             article_num += 1
 
-        # User selects an article to open.
         article_link_click = False
         while not article_link_click:
-            # If number of articles user wants to see is > than 1, take input as a list
-            # then iterate over each to open them.
             if num > 1:
                 selected_links = list(map(int, input(f'What links do you want to open? ').split()))
                 valid_links = 0
-                # Determine if all user entered indices exist
                 try:
                     for i in selected_links:
                         if 0 <= i < len(article_link) + 1:
                             valid_links += 1
                 except IndexError as i_err:
                     print(f'Err: {i_err}')
-                # All links are valid, and now we can open the links in the user's browser.
                 finally:
                     if valid_links == len(article_link):
                         for i in selected_links:
                             webbrowser.open(article_link[i - 1])
-            # Otherwise, we prompt the user to enter in the number again to visit the link.
             else:
                 single_link = int(input(f'Enter ({num}) again to open link: '))
                 webbrowser.open(article_link[single_link - 1])
 
-            # End while loop condition.
             article_link_click = True
 
-    # User has entered in an invalid website.
     except KeyError as k_err:
         print(f'Err: {k_err} is not a valid website')
         return
-    # User has entered in an article number that does not correspond to a stored index.
     except IndexError as i_err:
         print(f'Err: {i_err}')
         return
 
-
-# Return up to 30 of the latest CVEs based on user's input
 def cve_30(num):
-    # GET request to CVE website API
-    # Initialize list based on user's input
+    """
+    Display up to the last 30 CVEs on the terminal.
+    :param num: Number of CVES to display.
+    """
+
     content = requests.get("https://cve.circl.lu/api/last")
     json_elements = content.json()
     cve_list = list(json_elements[0:num])
 
-    # Format input and print to terminal
     for item in cve_list:
         print("{} {}".format("Vuln Num:", item['id']))
         print("{} {}\n".format("Desc:", item['summary']))
 
 
-# Return information about one of OWASP Top 10 vulnerabilities.
 def owasp_10():
-    # OWASP Top 10 links
+    """
+    Opens link on user's browser to the selected OWASP Top 10 vulnerability.
+    """
+
     websites = {
         "Broken Access Control": "https://owasp.org/Top10/A01_2021-Broken_Access_Control/",
         "Cryptographic Failures": "https://owasp.org/Top10/A02_2021-Cryptographic_Failures/",
@@ -112,12 +102,10 @@ def owasp_10():
         "Server-Side Request Forgery": "https://owasp.org/Top10/A10_2021-Server-Side_Request_Forgery_%28SSRF%29/",
     }
 
-    # Print title of OWASP Top 10
     print('OWASP Top 10')
     for k in websites.keys():
         print(k)
 
-    # Open link to OWASP Top 10 information
     link_click = False
     while not link_click:
         user_click = str(input("Type the security risk you would like to read more on.\n"))
@@ -126,6 +114,7 @@ def owasp_10():
 
 
 if __name__ == '__main__':
+
     print('Hello! Here are your options to select:\n- news: See latest cybersecurity news.\n'
           '- cve30: See up to 30 of the last.'
           'CVEs\n- owasp10: Links to OWASP Top 10 risks.')
